73829318e9f54c5dd6ec0e4d3ee521ba3c897692
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2022-12-13 16:12:13 +0100

    Add createPoster() function

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,26 @@
 import { movies } from "./movie-database.js";
+
+/* function to create a .movie-poster element
+ * @param {object} movie object from imported movies array
+ * @return {object} html element, poster
+ */
+const createPoster = function(movie) {
+  const imdbLink = `https://www.imdb.com/title/${movie.imdbID}/`;
+  //create elements
+  const poster = document.createElement("div");
+  const posterLink = document.createElement("a");
+  const posterImage = document.createElement("img");
+  // add "img" element to "a" element
+  posterLink.appendChild(posterImage);
+  // add "a" element (with nested img) to "div" element
+  poster.appendChild(posterLink);
+
+  //add attributes to elements
+  posterLink.setAttribute("href", imdbLink);
+  posterLink.setAttribute("target", "_blank");
+  posterImage.setAttribute("src", movie.poster);
+
+  // add .movie-poster class to poster-div
+  poster.classList.add("movie-poster");
+  return poster;
+};
