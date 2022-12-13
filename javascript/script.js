2fc6a00f2cb5381783ace66e66a1ab236abec8ca
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2022-12-13 21:51:09 +0100

    Add filter by date and name functions to script.js
    - add function filterMoviesByDate()
    - add function filterMoviesByName()

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,13 @@
+/* WINC FILM FINDER
+ * Assignment film finder
+ * Functionalities:
+ * - display a movie poster as gallery
+ * - filter movies (latest, avanger, x-men, princess, batman)
+ *   1 category at a time with radio buttons
+ * - clicking the poster opens coresponding imdb page
+ * BONUS feature(s):
+ * - search field to search by movie name */
+
 import { movies } from "./movie-database.js";
 
 /* function to create a .movie-poster element
@@ -31,3 +41,38 @@ const addPoster = function(movie) {
   const galery = document.getElementById("movie-galery");
   galery.appendChild(createPoster(movie));
 };
+
+/* function to filter movies by date,
+ * this can be used for the latest movies filter
+ * @param {array} - the imported movies array
+ * @param {string or int} - date, from year (of release) default: 1890
+ * @param {string or int} - date, until year (of release) default: current year
+ * If no argument(s) provided for date(s), default values are used
+ * steps:
+ * - filter array argument based on date arguments/ or defaults
+ * - loop through the sorted array and call addPoster() on each element */
+const filterMoviesByDate = (
+  moviesArr,
+  dateFrom = 1890,
+  dateUntil = new Date().getFullYear()
+) =>
+  moviesArr
+    .filter(
+      (movie) =>
+        Number(movie.year) >= Number(dateFrom) &&
+        Number(movie.year) <= Number(dateUntil)
+    )
+    .forEach((movie) => addPoster(movie));
+
+/* function to filter movies by name
+ * this can be used for the other 4 filter options (avang.,X-m.,prin., batm.)
+ * @param {array} - the imported movies array
+ * @param {string} - phrase, value of selected radio input
+ * If no argument(s) provided for date(s), default values are used
+ * steps:
+ * - filter array argument based on date arguments/ or defaults
+ * - loop through the sorted array and call addPoster() on each element */
+const filterMoviesByName = (moviesArr, phrase) =>
+  moviesArr
+    .filter((movie) => movie.title.includes(phrase))
+    .forEach((movie) => addPoster(movie));
