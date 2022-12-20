0061e7c0c3cb6ca2b7bf12db8870d5d90a42e908
tomlajtos <94200045+tomlajtos@users.noreply.github.com>
2022-12-20 09:42:51 +0100

    Update filter and search function add new search e.handler
    Update filter functions to work better with search functions
    
    - declare "currentYear" variable globally
    - add new event handler "searchHandler"
    
    - update createPoster: adds "gallery-item" class to posters
    - update clearGallery: use "galleryItems"{array} instead of "postersArr"
    - update filterMoviesByDate: option to filter for a single date (not a
      period)
    - update buildFilteredGallery: case "all-movies" replaces default
    - update searchInMovieTitle: passes arg "input.toLowerCase()" for filterf.
    - update "addListener..." functions

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,9 @@
 // - no need to rewrite filterMoviesByDate - only series have date as range (see next point)
 // - change filter functions to omit "series" type from movies array --> DONE
 // - implement search functionality
+//     - add a function that returns the number of filtered movies,
+//     - add function that displays a message if no search hit
+//     - add search/filter result info in footer ?
 // - add "show all movies" option to filtering --> DONE
 
 /* eslint-disable no-unused-vars */
@@ -16,16 +19,19 @@
  *    included in the results.
  * - clicking the poster opens corresponding imdb page
  * BONUS feature(s):
- * - search field to search by movie name */
+ * - search field to search by movie name
+ */
 
 import { movies } from "./movie-database.js";
 
 /* global variables for movie-gallery and search
  * to use with addPoster, clearGallery and
- * searchInMovieTitle functions */
+ * searchInMovieTitle functions
+ */
 const gallery = document.getElementById("movie-gallery");
 const searchBar = document.getElementById("movie-search");
 const searchButton = document.getElementById("search-button");
+const currentYear = new Date().getFullYear();
 
 // FUNCTIONS FOR FILTERING
 
@@ -33,7 +39,7 @@ const searchButton = document.getElementById("search-button");
  * @param {object} movie object from imported movies array
  * @return {object} html element, poster
  */
-const createPoster = function (movie) {
+const createPoster = function(movie) {
   const imdbLink = `https://www.imdb.com/title/${movie.imdbID}/`;
   //create elements
   const poster = document.createElement("div");
@@ -50,52 +56,68 @@ const createPoster = function (movie) {
   posterImage.setAttribute("src", movie.poster);
 
   // add .movie-poster class to poster-div
-  poster.classList.add("movie-poster");
+  poster.classList.add("movie-poster", "gallery-item");
   return poster;
 };
 
 /* function to add poster to the #poster-gallery element
-- * @param {object} movie object from imported movies array */
+ * @param {object} movie object from imported movies array
+ */
 const addPoster = (movie) => gallery.appendChild(createPoster(movie));
 
 /* function to clear the movie-gallery before a new filter is applied
- * removes each poster from  gallery if not empty */
-const clearGallery = function (name) {
-  const postersArr = Array.from(
-    document.getElementsByClassName("movie-poster")
+ * removes each poster from  gallery if not empty
+ */
+const clearGallery = function(name) {
+  const galleryItems = Array.from(
+    document.getElementsByClassName("gallery-item")
   );
+  console.log(galleryItems.length);
 
-  postersArr.length > 0
-    ? postersArr.forEach((poster) => gallery.removeChild(poster))
+  galleryItems.length > 0
+    ? galleryItems.forEach((item) => gallery.removeChild(item))
     : console.warn(
-        `WARNING: (disregard if first call of clearGallery function)
+      `WARNING: (disregard if first call of clearGallery function)
        Cannot clear movie-gallery of ${name}-movies, there are no child-elements to remove`,
-        gallery.children
-      );
+      gallery.children
+    );
 };
 
 /* function to filter movies by date,
  * this can be used for the latest movies filter
+ *
+ * - filter array argument based on date arguments/ or defaults
+ * - loop through the sorted array and call addPoster() on each element
+ *
  * @param {array} - the imported movies array
  * @param {string or int} - date, from year (of release) default: 1890
- * @param {string or int} - date, until year (of release) default: current year
- * If no argument(s) provided for date(s), default values are used
- * steps:
- * - filter array argument based on date arguments/ or defaults
- * - loop through the sorted array and call addPoster() on each element */
+ * @param {string, int or null} - date, until year (of release) default: current year
+ *   OR null: only movies matching dateFrom are filtered
+ * if no argument(s) provided for date(s), default values are used
+ */
 const filterMoviesByDate = (
   moviesArr,
   dateFrom = 1890,
-  dateUntil = new Date().getFullYear()
+  dateUntil = currentYear
 ) =>
-  moviesArr
-    .filter(
-      (movie) =>
-        Number(movie.year) >= Number(dateFrom) &&
-        Number(movie.year) <= Number(dateUntil) &&
-        movie.type === "movie"
-    )
-    .forEach((movie) => addPoster(movie));
+  // if dateUntil is null, dateFrom serves as exact year and only movies
+  // from that year will be added to the gallery
+  dateUntil === null
+    ? moviesArr
+      .filter(
+        (movie) =>
+          Number(movie.year) === Number(dateFrom) && movie.type === "movie"
+      )
+      .forEach((movie) => addPoster(movie))
+    : // otherwise adds movies within input/default range to gallery
+    moviesArr
+      .filter(
+        (movie) =>
+          Number(movie.year) >= Number(dateFrom) &&
+          Number(movie.year) <= Number(dateUntil) &&
+          movie.type === "movie"
+      )
+      .forEach((movie) => addPoster(movie));
 
 /* function to filter movies by name
  * this can be used for the other 4 filter options (avang.,X-m.,prin., batm.)
@@ -114,10 +136,13 @@ const filterMoviesByName = (moviesArr, phrase) => {
 
 /* function to show all movies (series are ignored)
  * if radio-input #all-movies has checked attr.:
- * -> filterMoviesByDate - with default params.
+ *
+ * - filterMoviesByDate - with default params.
  * - show warning for dev purposes if no checked attr.
+ *
  * IMPORTANT: call function in global context to have
- * movie finder show all movies by default */
+ * movie finder show all movies by default
+ */
 const showAllMovies = (moviesArr) => {
   const filterForAll = document.getElementById("all-movies");
 
@@ -128,12 +153,14 @@ const showAllMovies = (moviesArr) => {
 
 /* function to add the posters of each movie
  * that fits the selected filter to the movie-gallery
- * shows all movies as default option
+ *
  * @param {array} - the imported movies array
  * @param {string} - phrase, value of selected radio input
+ *
  * (called by the anonym event handler on radio-button
- * change event) */
-const buildFilteredGallery = function (moviesArr, phrase) {
+ * change event)
+ */
+const buildFilteredGallery = function(moviesArr, phrase) {
   clearGallery(phrase);
   // call filter functions based on input-value (phrase)
   switch (phrase) {
@@ -152,7 +179,7 @@ const buildFilteredGallery = function (moviesArr, phrase) {
     case "batman":
       filterMoviesByName(moviesArr, "batman");
       break;
-    default:
+    case "all-movies":
       showAllMovies(moviesArr);
   }
 };
@@ -162,15 +189,40 @@ const buildFilteredGallery = function (moviesArr, phrase) {
 /* function to search for movies by checking if
  * search bar input-value is part of the movie title
  * @param {array} - the imported movies array
- * - gets searchBar value and passes it into
- *   filterMoviesByName function as arg.
- * - called by the anonym event handler function
- *   for search-button click event */
-const searchInMovieTitle = function (moviesArr) {
-  const input = searchBar.value.trim();
-
+ * @param {string} - user input from search
+ *
+ * - calles filterMoviesByName function
+ */
+const searchInMovieTitle = function(moviesArr, input) {
   clearGallery(input);
-  filterMoviesByName(moviesArr, input);
+
+  filterMoviesByName(moviesArr, input.toLowerCase());
+};
+
+/* function that calls the appropriate search function based on the
+ * input type
+ *
+ * - check if search input is text
+ * - check in case input is string of digits that if it is a valid date:
+ *   if not, search in title (could be improved for movie titles which are
+ *   valid as release date as well to filter for both title/date.
+ *   i.e. "1984" - I didn't do it however...)
+ *
+ * this function handles key-press and click events on search
+ */
+const searchHandler = function() {
+  const searchInput = searchBar.value.trim();
+  const dateRegEx = /(\d{4})+/g; //to be used with date search function
+  const textRegEx = /[a-z]+/gi;
+  const inputHasText = textRegEx.test(searchInput);
+  const notValidDate = searchInput
+    .match(/\d+/g)
+    .every((num) => num > currentYear || num < 1890);
+  console.log("search input is not a movie date:", notValidDate);
+
+  inputHasText || notValidDate
+    ? searchInMovieTitle(movies, searchInput)
+    : console.warn("search input is not for title");
 };
 
 // FUNCTIONS FOR EVENT LISTENERS
@@ -179,7 +231,7 @@ const searchInMovieTitle = function (moviesArr) {
  * loop through an array of filter-radio-button elements
  * add change event to each
  * use buildFilteredGallery() as event handler */
-const addListenerToFilterButtons = function () {
+const addListenerToFilterButtons = function() {
   const filterButtonArr = Array.from(
     document.getElementsByClassName("filter-button")
   );
@@ -195,14 +247,14 @@ const addListenerToFilterButtons = function () {
 
 /* function to add event listener to search button ("click")
  * and search bar ("keypress") */
-const addListenersToSearch = function () {
+const addListenersToSearch = function() {
   searchBar.addEventListener("keypress", (e) => {
-    e.key === "Enter" ? searchInMovieTitle(movies) : e.key;
+    if (e.key === "Enter") {
+      searchHandler();
+    }
   });
 
-  searchButton.addEventListener("click", () => {
-    searchInMovieTitle(movies);
-  });
+  searchButton.addEventListener("click", searchHandler);
 };
 
 // CALL FUNCTIONS
